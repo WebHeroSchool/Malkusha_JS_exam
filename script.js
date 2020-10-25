f5c50beafafc53184ff7d44b27f493ade00ec155
Nastya <malkusha@mail.ru>
2020-10-25 19:25:00 +0300

    mobile-version

--- script.js
+++ script.js
@@ -1,46 +1,33 @@
 let field = document.getElementById('field');
 let title = document.getElementById('header');
-let level = document.getElementById('level');
-let choice, line, card, front, back, result;
-let playGame = true;
-
-level.addEventListener('click', chooseLevel => {
-  if (choice) {
-    choice.classList.toggle('start__level_chosen');
-  };
-  choice = chooseLevel.target;
-  choice.classList.toggle('start__level_chosen');
-});
+let level = document.getElementsByClassName('start__choose');
+let choice, line, card, front, back, backImg, frontImg, result;
+for (let l of level) {
+  l.addEventListener('click', chooseLevel => {
+    if (choice) {
+      choice.classList.toggle('start__chosen');
+    };
+    choice = chooseLevel.target;
+    choice.classList.toggle('start__chosen');
+  });
+}
 
 let btn = document.getElementById('button');
 btn.addEventListener('click', start => {
-  choice.classList.toggle('start__level_chosen');
+  choice.classList.toggle('start__chosen');
   title.classList.toggle('start_then');
   field.classList.toggle('start_then');
   document.body.classList.toggle('game');
   (choice.id === 'easy') ? getEasy():
   (choice.id === 'normal') ? getNormal():
   getHard();
+  console.log(front);
+  console.log(frontImg);
   let cards = document.querySelectorAll('.game__img');
-  console.log(cards);
   for(let i of cards) {
-    i.addEventListener('mouseover', getHover => {
-      let hover = getHover.target;
-      hover.classList.toggle('game__img_hover');
-    });
-    i.addEventListener('mouseout', getHover => {
-      let hover = getHover.target;
-      hover.classList.toggle('game__img_hover');
-    });
     i.addEventListener('click', check => {
       choice = check.target;
-      choice.id = 'back';
-      back = document.querySelector('#back ~ div')
-      result = getRandomInt(2);
-      (result === 0) ? back.classList.add('game__front_false'):
-      back.classList.add('game__front_true');
       let rotate = choice.closest('.game__img');
-      console.log(rotate);
       rotate.classList.toggle('game__img_change');
       for (let card of cards) {
         card.addEventListener('click', getNewGame => {
@@ -54,12 +41,8 @@ btn.addEventListener('click', start => {
                 })
       }
     });
-
-
   }
 });
-/*
- */
 
 let makeLine = () => {
   line = document.createElement('div');
@@ -74,31 +57,68 @@ let getCards = () => {
   back = document.createElement('div');
   back.classList.toggle('game__back');
   card.append(back);
+  backImg = document.createElement('img');
+  backImg.src = 'Img/Back.png';
+  back.append(backImg);
+  backImg.classList.toggle('game__pic');
   front = document.createElement('div');
   back.after(front);
   front.classList.toggle('game__front');
+  frontImg = document.createElement('img');
+  result = getRandomInt(2);
+  (result === 0) ? frontImg.src = 'Img/GameOver.png':
+  frontImg.src = 'Img/Bug.png';
+  front.append(frontImg);
+  frontImg.classList.toggle('game__result');
 };
 
 let getEasy = () => {
-  makeLine();
-  for (let i = 0; i < 3; i++) {
-    getCards();
+  if (window.innerWidth <= 375) {
+    for (let k = 0; k < 3; k++) {
+      makeLine();
+      getCards();
+  }
+  } else {
+    makeLine();
+    for (let i = 0; i < 3; i++) {
+      getCards();
+    }
   }
 };
 
 let getNormal = () => {
-  for (let k = 0; k<2; k++) {
-    getEasy();
+  if (window.innerWidth <= 375) {
+    for (let k = 0; k < 3; k++) {
+      makeLine();
+      for (let i = 0; i < 2; i++) {
+        getCards();
+      }
+    }
+  } else {
+    for (let k = 0; k < 2; k++) {
+      getEasy();
+    }
   }
+
 };
 
 let getHard = () => {
-  for (let k = 0; k<2; k++) {
-    makeLine();
-    for (let i = 0; i < 5; i++) {
-      getCards();
+  if (window.innerWidth <= 375) {
+    for (let k = 0; k < 5; k++) {
+      makeLine();
+      for (let i = 0; i < 2; i++) {
+        getCards();
+      }
+    }
+  } else {
+    for (let k = 0; k < 2; k++) {
+      makeLine();
+      for (let i = 0; i < 5; i++) {
+        getCards();
+      }
     }
   }
+
 };
 
 function getRandomInt(max) {

c93718a6bc9c6899bdbc66abab84624d2c82938b
Nastya <malkusha@mail.ru>
2020-10-26 19:37:51 +0300

    functions

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 let field = document.getElementById('field');
 let title = document.getElementById('header');
-let level = document.getElementsByClassName('start__choose');
-let choice, line, card, front, back, backImg, frontImg, result;
-for (let l of level) {
-  l.addEventListener('click', chooseLevel => {
+let difficulty = document.getElementsByClassName('start__choose');
+let choice, cardChoice, line, card, result;
+for (let e of difficulty) {
+  e.addEventListener('click', chooseDifficulty => {
     if (choice) {
       choice.classList.toggle('start__chosen');
     };
-    choice = chooseLevel.target;
+    choice = chooseDifficulty.target;
     choice.classList.toggle('start__chosen');
   });
 }
@@ -21,14 +21,15 @@ btn.addEventListener('click', start => {
   (choice.id === 'easy') ? getEasy():
   (choice.id === 'normal') ? getNormal():
   getHard();
+  choice = null;
   let cards = document.querySelectorAll('.game__img');
-  for(let i of cards) {
-    i.addEventListener('click', check => {
-      choice = check.target;
-      let rotate = choice.closest('.game__img');
+  for(let e of cards) {
+    e.addEventListener('click', check => {
+      cardChoice = check.target;
+      let rotate = cardChoice.closest('.game__img');
       rotate.classList.toggle('game__img_change');
-      for (let card of cards) {
-        card.addEventListener('click', getNewGame => {
+      for (let ev of cards) {
+        ev.addEventListener('click', getNewGame => {
                   let newGame = document.querySelectorAll('div');
                   for (let k of newGame) {
                     k.remove();
@@ -52,34 +53,52 @@ let getCards = () => {
   card = document.createElement('div');
   line.append(card);
   card.classList.toggle('game__img');
-  back = document.createElement('div');
+  getBackCardImg(getBackCard);
+  getFrontCardImg(getFrontCard);
+}
+
+let getBackCard = () => {
+  let back = document.createElement('div');
   back.classList.toggle('game__back');
   card.append(back);
-  backImg = document.createElement('img');
+  return back;
+}
+
+let getBackCardImg = () => {
+  let backImg = document.createElement('img');
   backImg.src = 'Img/Back.png';
-  back.append(backImg);
+  getBackCard().append(backImg);
   backImg.classList.toggle('game__pic');
-  front = document.createElement('div');
-  back.after(front);
+  return backImg;
+}
+
+let getFrontCard = () => {
+  let front = document.createElement('div');
+  card.append(front);
   front.classList.toggle('game__front');
-  frontImg = document.createElement('img');
+  return front;
+}
+
+let getFrontCardImg = () => {
+  let frontImg = document.createElement('img');
   result = getRandomInt(2);
   (result === 0) ? frontImg.src = 'Img/GameOver.png':
   frontImg.src = 'Img/Bug.png';
-  front.append(frontImg);
+  getFrontCard().append(frontImg);
   frontImg.classList.toggle('game__result');
-};
+  return frontImg;
+}
 
 let getEasy = () => {
   if (window.innerWidth <= 375) {
     for (let k = 0; k < 3; k++) {
       makeLine();
-      getCards();
+      getCards(makeLine);
   }
   } else {
     makeLine();
     for (let i = 0; i < 3; i++) {
-      getCards();
+      getCards(makeLine);
     }
   }
 };
@@ -89,7 +108,7 @@ let getNormal = () => {
     for (let k = 0; k < 3; k++) {
       makeLine();
       for (let i = 0; i < 2; i++) {
-        getCards();
+        getCards(makeLine);
       }
     }
   } else {
@@ -105,20 +124,19 @@ let getHard = () => {
     for (let k = 0; k < 5; k++) {
       makeLine();
       for (let i = 0; i < 2; i++) {
-        getCards();
+        getCards(makeLine);
       }
     }
   } else {
     for (let k = 0; k < 2; k++) {
       makeLine();
       for (let i = 0; i < 5; i++) {
-        getCards();
+        getCards(makeLine);
       }
     }
   }
-
 };
 
-function getRandomInt(max) {
+let getRandomInt = (max) => {
   return Math.floor(Math.random() * Math.floor(max));
 };
